bc76824eb46084c0a240f02757dbe0df0b42412c
Zach <zacharyjordan82@gmail.com>
2020-06-03 11:05:05 -0400

    front page styles

--- tcg-theme/front-page.php
+++ tcg-theme/front-page.php
@@ -9,7 +9,7 @@
       <div class="carousel-inner" role="listbox">
 
         <!-- Slide 1 -->
-        <div class="carousel-item active" style="background-image: url(assets/img/slide/slide-1.jpg)">
+        <div class="carousel-item active" style="background-image: url(wp-content/uploads/2020/06/slide-1.jpg)">
           <div class="carousel-container">
             <div class="container">
               <h2 class="animate__animated animate__fadeInDown">Welcome to <span>Green</span></h2>
@@ -20,7 +20,7 @@
         </div>
 
         <!-- Slide 2 -->
-        <div class="carousel-item" style="background-image: url(assets/img/slide/slide-2.jpg)">
+        <div class="carousel-item" style="background-image: url(wp-content/uploads/2020/06/slide-2.jpg)">
           <div class="carousel-container">
             <div class="container">
               <h2 class="animate__animated animate__fadeInDown">Lorem Ipsum Dolor</h2>
@@ -31,7 +31,7 @@
         </div>
 
         <!-- Slide 3 -->
-        <div class="carousel-item" style="background-image: url(assets/img/slide/slide-3.jpg)">
+        <div class="carousel-item" style="background-image: url(wp-content/uploads/2020/06/slide-3.jpg)">
           <div class="carousel-container">
             <div class="container">
               <h2 class="animate__animated animate__fadeInDown">Sequi ea ut et est quaerat</h2>
@@ -55,5 +55,123 @@
 
     </div>
   </section><!-- End Hero -->
+  <section id="featured-services" class="featured-services section-bg">
+      <div class="container">
+
+        <div class="row no-gutters">
+          <div class="col-lg-4 col-md-6">
+            <div class="icon-box">
+              <div class="icon"><i class="icofont-computer"></i></div>
+              <h4 class="title"><a href="">Lorem Ipsum</a></h4>
+              <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6">
+            <div class="icon-box">
+              <div class="icon"><i class="icofont-image"></i></div>
+              <h4 class="title"><a href="">Dolor Sitema</a></h4>
+              <p class="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata</p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6">
+            <div class="icon-box">
+              <div class="icon"><i class="icofont-tasks-alt"></i></div>
+              <h4 class="title"><a href="">Sed ut perspiciatis</a></h4>
+              <p class="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur trade stravi</p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Featured Services Section -->
+
+    <!-- ======= About Us Section ======= -->
+    <section id="about" class="about">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>About Us</h2>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-6 order-1 order-lg-2">
+            <img src="wp-content/uploads/2020/06/about.jpg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content">
+            <h3>THE CROSSROADS GROUP, LLC</h3>
+            <p>
+              THE CROSSROADS GROUP, LLC (TCG), is a full-service civil engineering and land
+surveying firm headquartered in Hamburg, Pennsylvania. The Crossroads Group, LLC has
+performed civil engineering and land surveying throughout the Commonwealth of
+Pennsylvania, as well as project evaluations and designs in New York, Maryland, Delaware
+and West Virginia, New Jersey, Ohio, Virginia, and North Carolina. Our clientele ranges
+from single-family homeowners in need of grading and building permits, to nationally
+recognized telecommunication and energy companies in need of site surveying, design and
+permits. While our clients and their respective needs may vary, our high level of service
+does not. Every project is treated with the utmost level of engineering and project
+management excellence.
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Us Section -->
+
+    <!-- ======= Why Us Section ======= -->
+    <section id="why-us" class="why-us">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 p-3 text-center">
+            <p>TCG recognizes and addresses the various needs of our clients and the community. It is
+              this flexibility; coupled with our attention to each client’s individual needs that distinguishes
+              us from other engineering and land surveying firms. Our diverse client base includes:</p>
+          </div>
+        </div>
+        <div class="row no-gutters">
+
+          <div class="col-lg-4 col-md-6 content-item">
+            <h4>Residential Land Developers</h4>
+            <p>Residential land developers in need of fully engineered subdivision plans and
+              associated permits.</p>
+          </div>
+
+          <div class="col-lg-4 col-md-6 content-item">
+            <h4>Commercial Land Developers</h4>
+            <p>Commercial land developers in need of fully engineered land development
+              plans and associated permits.</p>
+          </div>
+
+          <div class="col-lg-4 col-md-6 content-item">
+            <h4>Non Profit Environmental Organizations</h4>
+            <p>Non-profit environmental organizations requiring fully engineered flood plain
+              studies, reforestation surveys and wetland and/or groundwater recharge
+              designs.</p>
+          </div>
+
+          <div class="col-lg-4 col-md-6 content-item">
+            <h4>Institutional Organizations</h4>
+            <p>Institutional organizations and places of worship in need of fully engineered
+              land development plans and associated permits.</p>
+          </div>
+
+          <div class="col-lg-4 col-md-6 content-item">
+            <h4>Energy Companies</h4>
+            <p>Energy companies in need of site survey, site access design, erosion, and
+              sedimentation control plans, and associated municipal, county and state
+              permitting.</p>
+          </div>
+
+          <div class="col-lg-4 col-md-6 content-item">
+            <h4>Telecommunication Companies</h4>
+            <p>Telecommunication companies in need of site survey, lease exhibit plans,
+              zoning plans, construction drawings and supporting PADEP land development
+              approvals.</p>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Why Us Section -->
+
 
 <?php get_footer();?>
