398d25da1afc2d3ec94551e3457c07fe65d2cccf
Zachary Lockhart <zacharyjordan82@gmail.com>
2026-07-01 16:08:10 -0400

    removing pages

--- tcg-theme/page.php
+++
@@ -1,7 +0,0 @@
-<?php get_header();?>
-<div class="mt-3">&nbsp;</div>
-<div class="mt-sm-3">&nbsp;</div>
-<?php if(have_posts()): while(have_posts()): the_post();?>
-        <?php the_content();?>
-<?php endwhile; endif;?>
-<?php get_footer();?>
